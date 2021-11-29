668e9cd53f4140bcff0bd79895b17fcc33418109
Alan Andreoni <s20079023@students.duvalschools.org>
2021-11-29 09:07:23 -0500

    v0.3
    Fonts

--- PyGamePractice.py
+++ PyGamePractice.py
@@ -1,4 +1,4 @@
-# Pygame Practice, Alan Andreoni, 11/29/2021 8:35 am, v0.2
+# Pygame Practice, Alan Andreoni, 11/29/2021 8:35 am, v0.3
 
 import pygame, sys
 from pygame.locals import *
@@ -8,7 +8,7 @@ pygame.init()
 
 # Create game window
 windowSurface = pygame.display.set_mode((500, 400), 0, 32)
-pygame.display.set_caption("Hello, World!")
+pygame.display.set_caption('Hello, World!')
 
 # Set Color Values
 BLACK = (0, 0, 0)
@@ -16,3 +16,10 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+# Setup Fonts
+basicFont = pygame.font.SysFont(None, 48)
+
+#Setup Text
+text = basicFont.render('Hello, World!', True, WHITE, BLUE)
+
