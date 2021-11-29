ba23a170f0992746d7c18d192c51428212ede724
Alan Andreoni <s20079023@students.duvalschools.org>
2021-11-29 09:02:24 -0500

    v0.2
    Color Values

--- PyGamePractice.py
+++ PyGamePractice.py
@@ -1,5 +1,18 @@
-# Pygame Practice, Alan Andreoni, 11/29/2021 8:35 am, v0.1
+# Pygame Practice, Alan Andreoni, 11/29/2021 8:35 am, v0.2
 
 import pygame, sys
 from pygame.locals import *
 
+# start pygame
+pygame.init()
+
+# Create game window
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption("Hello, World!")
+
+# Set Color Values
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
