bc98989d8aaafc2c1a31216835d5b77cabbe5b11
Alan Andreoni <s20079023@students.duvalschools.org>
2021-11-29 09:30:15 -0500

    v0.7
    Added a circle and an ellipse

--- PyGamePractice.py
+++ PyGamePractice.py
@@ -36,3 +36,9 @@ pygame.draw.polygon(windowSurface, GREEN, ((146,0), (291, 106), (236, 277), (56,
 pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
 pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
 pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
+
+# Draw a circle
+pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
+
+# Draw an ellipse
+pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
