deda796e50c714dae3e4417f8ed626d641642fd6
Alan Andreoni <s20079023@students.duvalschools.org>
2021-12-08 18:59:26 -0500

    v0.1
    Imported modules for pygame, system, and time,
    
    inialized the pygame module
    
    setup window diminsions.

--- simple_animation.py
+++ simple_animation.py
@@ -1,1 +1,13 @@
-# Simple Animation with Pygame, Alan Andreoni, 12/8/2021, 6:54 pm v0.0
+# Simple Animation with Pygame, Alan Andreoni, 12/8/2021, 6:58 pm v0.1
+
+import pygame, sys, time
+from pygame.locals import *
+
+# Setup Pygame
+pygame.innit()
+
+# Setup the window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caltion('Animation Example!')
