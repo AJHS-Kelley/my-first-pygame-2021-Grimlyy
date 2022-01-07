3a90bff58670b65f09541791d02f101d8a2cffce
Alan Andreoni <s20079023@students.duvalschools.org>
2022-01-07 18:04:02 -0500

    v0.1
    Imported the pygame system and random modules
    
    Setup pygame and create mainclock manual

--- collision_detection.py
+++ collision_detection.py
@@ -1,1 +1,9 @@
-#Pygame Collision Dection Practice, Alan Andreoni, 1/7/22 5:51 pm, v0.0
+#Pygame Collision Dection Practice, Alan Andreoni, 1/7/22 6:03 pm, v0.1
+
+import pygame, sys, random
+from pygame.locals import *
+
+# Setup PyGame
+pygame.init()
+mainClock = pygame.time.Clock()
+
